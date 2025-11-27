c67319d38d8d75a5919c6df1b49cfa3a61016b17
Alex Thompson <alexanderkthompson@gmail.com>
2025-11-27 08:32:51 +0000

    Added a column preset

---
+++ ResearchPlatform/ResearchProject_WwiseProject/GeneratedSoundBanks/Windows/Wwise_IDs.h
@@ -0,0 +1,331 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID EVENT_UI_SELECT_AUDIOCRUMBSPROPERTIES = 3831725167U;
+        static const AkUniqueID EVENT_UI_SELECT_ECHOLOCATORCLICKPROPERTIES = 2040130348U;
+        static const AkUniqueID EVENT_UI_SELECT_FOOTSTEPSPROPERTIES = 2286525692U;
+        static const AkUniqueID EVENT_UI_SELECT_LANDMARKSPROPERTIES = 1132456374U;
+        static const AkUniqueID EVENT_UI_SELECT_MAINSETTINGS = 3833221374U;
+        static const AkUniqueID EVENT_UI_SELECT_MULTIPURPOSE = 222667337U;
+        static const AkUniqueID EVENT_UI_SELECT_OBSTACLESPROPERTIES = 2953026179U;
+        static const AkUniqueID EVENT_UI_SELECT_SIMPLETUTORIAL = 2256606490U;
+        static const AkUniqueID PAUSE_TUTORIAL = 1193160806U;
+        static const AkUniqueID PLAY_01_NV_WELCOME = 1784930419U;
+        static const AkUniqueID PLAY_02_NV_ECHOCLICK = 1517633171U;
+        static const AkUniqueID PLAY_03_NV_FORWARDMOVEMENT_PT = 506850176U;
+        static const AkUniqueID PLAY_04_NV_COLLISIONS_PT = 1113817490U;
+        static const AkUniqueID PLAY_05_NV_CONTINUEDCOLLISIONS = 1602792029U;
+        static const AkUniqueID PLAY_06_NV_BACKWARDS = 3644984316U;
+        static const AkUniqueID PLAY_07_NV_SIDEWAYS = 1904249682U;
+        static const AkUniqueID PLAY_08_NV_MOVEMENTPRACTICE_PT = 3411623047U;
+        static const AkUniqueID PLAY_09_NV_ROTATIONINTRO = 2944014995U;
+        static const AkUniqueID PLAY_10_NV_SONICCOMPASS_PT = 3902747138U;
+        static const AkUniqueID PLAY_11_NV_ROTATIONANDMOVEMENTPRACTICE = 584364295U;
+        static const AkUniqueID PLAY_12_NV_SPAWNATSTART_PT = 460292358U;
+        static const AkUniqueID PLAY_13_NV_MAINCHAMBERSPAWN_PT = 2220317695U;
+        static const AkUniqueID PLAY_14_NV_SPAWNPOINTSANDMOVEMENT = 1944555383U;
+        static const AkUniqueID PLAY_15_NV_REVIEW = 1344036906U;
+        static const AkUniqueID PLAY_16_NV_UIINTRO_PT = 3224347966U;
+        static const AkUniqueID PLAY_17_NV_UIMAINMENUNAV = 805288807U;
+        static const AkUniqueID PLAY_18_NV_ENTERINGACATEGORY = 2496975038U;
+        static const AkUniqueID PLAY_19_NV_NAVIGATINGUISUBMENUS_PT = 2828457565U;
+        static const AkUniqueID PLAY_20_NV_ADJUSTINGSLIDERS = 2975437661U;
+        static const AkUniqueID PLAY_21_NV_UIREVIEW = 2552998629U;
+        static const AkUniqueID PLAY_22_NV_OBSTACLESPRESETS = 774137046U;
+        static const AkUniqueID PLAY_23_NV_OBSTACLESCUSTOMIZATION = 1257884420U;
+        static const AkUniqueID PLAY_24_NV_OBSTACLESNAVIGATION = 3789614610U;
+        static const AkUniqueID PLAY_25_NV_HEARINGTHEOBSTACLES_PT = 1835230543U;
+        static const AkUniqueID PLAY_26_NV_OBSTACLEPRESETDESCRIPTIONS = 693311155U;
+        static const AkUniqueID PLAY_27_NV_OBSTACLESREVIEW = 3863533881U;
+        static const AkUniqueID PLAY_28_NV_AUDIOLANDMARKS_PT = 1959403158U;
+        static const AkUniqueID PLAY_29_NV_LANDMARKS_VOLUMESLIDERS = 1945207839U;
+        static const AkUniqueID PLAY_30_NV_TUTORIALITEMSANDCONCLUSION = 2609550161U;
+        static const AkUniqueID PLAY_45_ANGLE_INDICATOR = 2248556949U;
+        static const AkUniqueID PLAY_AUDIOCRUMBSPROPERTIESPAGE = 847023378U;
+        static const AkUniqueID PLAY_COLLISION = 1553219250U;
+        static const AkUniqueID PLAY_COMPASS_SYNTH = 2570030149U;
+        static const AkUniqueID PLAY_DEFINITELYNOTSAMECLOCK = 481676882U;
+        static const AkUniqueID PLAY_DEFINITELYNOTSAMEHVAC = 1269349234U;
+        static const AkUniqueID PLAY_EAST = 4162345221U;
+        static const AkUniqueID PLAY_ECHOLOCATORCLICKPROPERTIESPAGE = 3738316925U;
+        static const AkUniqueID PLAY_ENVIRONMENT_BETTERCLOCK = 1504398712U;
+        static const AkUniqueID PLAY_ENVIRONMENT_BETTERCLOCK_2D = 2175879169U;
+        static const AkUniqueID PLAY_ENVIRONMENT_HVAC = 3548695574U;
+        static const AkUniqueID PLAY_ENVIRONMENT_HVAC_2D = 859895883U;
+        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+        static const AkUniqueID PLAY_FOOTSTSEPSPROPERTIESPAGE = 587051210U;
+        static const AkUniqueID PLAY_HUMANCLICK = 3559566211U;
+        static const AkUniqueID PLAY_LANDMARKSPROPERTIESPAGE = 3772977147U;
+        static const AkUniqueID PLAY_MAINCHAMBERNOTIFICATION = 1839782124U;
+        static const AkUniqueID PLAY_MAINGAME = 1833603863U;
+        static const AkUniqueID PLAY_MAINSETTINGSINITIALOPEN = 2142858666U;
+        static const AkUniqueID PLAY_NORTH = 1850732987U;
+        static const AkUniqueID PLAY_OBSTACLEEMITTER = 2173823837U;
+        static const AkUniqueID PLAY_OBSTACLESPROPERTIESPAGE = 3953798054U;
+        static const AkUniqueID PLAY_SETTINGSCLOSED = 514607133U;
+        static const AkUniqueID PLAY_SOUTH = 3187449017U;
+        static const AkUniqueID PLAY_STARTINGPOSITIONNOTIFICATION = 2531445826U;
+        static const AkUniqueID PLAY_TUTORIALITEMSPAGE = 2321440389U;
+        static const AkUniqueID PLAY_WEST = 1103419735U;
+        static const AkUniqueID RESUME_TUTORIAL = 4041214897U;
+        static const AkUniqueID STOP_COMPASS_SYNTH = 3787876787U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace TUTORIAL_STATE
+        {
+            static const AkUniqueID GROUP = 587064525U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID TUTORIAL_PAUSED = 3625923408U;
+                static const AkUniqueID TUTORIAL_PLAYING = 126890676U;
+            } // namespace STATE
+        } // namespace TUTORIAL_STATE
+
+        namespace UI_AUDIO_STATE
+        {
+            static const AkUniqueID GROUP = 1875554370U;
+
+            namespace STATE
+            {
+                static const AkUniqueID IDLE = 1874288895U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID WINDOW_OPENING = 1592121920U;
+            } // namespace STATE
+        } // namespace UI_AUDIO_STATE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace SG_UI_AUDIOCRUMBSSELECTIONS
+        {
+            static const AkUniqueID GROUP = 2355358046U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID AUDIOCRUMBS_ON_CHECKED = 2929736493U;
+                static const AkUniqueID AUDIOCRUMBS_ON_NOTCHECKED = 756878500U;
+                static const AkUniqueID AUDIOCRUMBS_VOLUMESLIDER = 1663067993U;
+            } // namespace SWITCH
+        } // namespace SG_UI_AUDIOCRUMBSSELECTIONS
+
+        namespace SG_UI_ECHOLOCATORCLICKSELECTIONS
+        {
+            static const AkUniqueID GROUP = 3225717335U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID PITCHSLIDER = 948093630U;
+                static const AkUniqueID VOLUMESLIDER = 2955768760U;
+            } // namespace SWITCH
+        } // namespace SG_UI_ECHOLOCATORCLICKSELECTIONS
+
+        namespace SG_UI_FOOTSTEPSSELECTION
+        {
+            static const AkUniqueID GROUP = 4172979502U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID FOOTSTEPS_ON_CHECKED = 1668538352U;
+                static const AkUniqueID FOOTSTEPS_ON_NOTCHECKED = 616568539U;
+                static const AkUniqueID FOOTSTEPS_PITCHSLIDER = 1792357110U;
+                static const AkUniqueID FOOTSTEPS_VOLUMESLIDER = 3880767216U;
+            } // namespace SWITCH
+        } // namespace SG_UI_FOOTSTEPSSELECTION
+
+        namespace SG_UI_LANDMARKSSELECTIONS
+        {
+            static const AkUniqueID GROUP = 2201101635U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CLOCKON_CHECKED = 4145643500U;
+                static const AkUniqueID CLOCKON_NOTCHECKED = 4105537559U;
+                static const AkUniqueID CLOCKVOLUME_SLIDER = 202463777U;
+                static const AkUniqueID HVACON_CHECKED = 3394946734U;
+                static const AkUniqueID HVACON_NOTCHECKED = 2726150469U;
+                static const AkUniqueID HVACVOLUME_SLIDER = 3003445939U;
+            } // namespace SWITCH
+        } // namespace SG_UI_LANDMARKSSELECTIONS
+
+        namespace SG_UI_MAINSETTINGS
+        {
+            static const AkUniqueID GROUP = 3198805459U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ACP_5OF6 = 1110678646U;
+                static const AkUniqueID ACP_CAG_5OF6 = 3635598136U;
+                static const AkUniqueID ECP_1OF6 = 1332976950U;
+                static const AkUniqueID ECP_CAG_1OF6 = 1311420848U;
+                static const AkUniqueID FSP_3OF6 = 1083307317U;
+                static const AkUniqueID FSP_CAG_3OF6 = 3823066771U;
+                static const AkUniqueID LMP_4OF6 = 3051212308U;
+                static const AkUniqueID LMP_CAG_4OF6 = 1710388822U;
+                static const AkUniqueID OBS_2OF6 = 1853522097U;
+                static const AkUniqueID OBS_CAG_2OF6 = 1570397931U;
+                static const AkUniqueID TTI_6OF6 = 1830141040U;
+                static const AkUniqueID TTI_CAG_6OF6 = 1312921442U;
+            } // namespace SWITCH
+        } // namespace SG_UI_MAINSETTINGS
+
+        namespace SG_UI_MULTIPURPOSE
+        {
+            static const AkUniqueID GROUP = 2014534092U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID TOGGLE_CHECKED = 1767045039U;
+                static const AkUniqueID TOGGLE_NOTCHECKED = 4150790098U;
+            } // namespace SWITCH
+        } // namespace SG_UI_MULTIPURPOSE
+
+        namespace SG_UI_OBSTACLESELECTIONS
+        {
+            static const AkUniqueID GROUP = 3969249293U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CARPET_CHECKED = 3357802622U;
+                static const AkUniqueID CARPET_NOTCHECKED = 702030869U;
+                static const AkUniqueID CONCRETE_CHECKED = 3708900630U;
+                static const AkUniqueID CONCRETE_NOTCHECKED = 3547507741U;
+                static const AkUniqueID CT_CHECKED = 3092067370U;
+                static const AkUniqueID CT_NOTCHECKED = 3934324673U;
+                static const AkUniqueID DEFAULT_CHECKED = 2618201234U;
+                static const AkUniqueID DEFAULT_NOTCHECKED = 1998357529U;
+                static const AkUniqueID LA_CHECKED = 3468833026U;
+                static const AkUniqueID LA_NOTCHECKED = 106420105U;
+                static const AkUniqueID LS_CHECKED = 2366001860U;
+                static const AkUniqueID LS_NOTCHECKED = 2033501439U;
+                static const AkUniqueID MA_CHECKED = 3763739957U;
+                static const AkUniqueID MA_NOTCHECKED = 1197290428U;
+                static const AkUniqueID MAS_CHECKED = 3883336016U;
+                static const AkUniqueID MAS_NOTCHECKED = 3612480955U;
+                static const AkUniqueID MR_CHECKED = 234490334U;
+                static const AkUniqueID MR_NOTCHECKED = 3990053621U;
+                static const AkUniqueID MS_CHECKED = 2857123331U;
+                static const AkUniqueID MS_NOTCHECKED = 2249676422U;
+                static const AkUniqueID SS_CHECKED = 1048812117U;
+                static const AkUniqueID SS_NOTCHECKED = 4001707100U;
+                static const AkUniqueID VSS_CHECKED = 1564979737U;
+                static const AkUniqueID VSS_NOTCHECKED = 3073952096U;
+            } // namespace SWITCH
+        } // namespace SG_UI_OBSTACLESELECTIONS
+
+        namespace SG_UI_SIMPLETUTORIAL
+        {
+            static const AkUniqueID GROUP = 3723283095U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID END = 529726532U;
+                static const AkUniqueID START = 1281810935U;
+            } // namespace SWITCH
+        } // namespace SG_UI_SIMPLETUTORIAL
+
+        namespace SG_UI_TUTORIALITEMS
+        {
+            static const AkUniqueID GROUP = 864818103U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ADJUSTINGSLIDERS = 3537363534U;
+                static const AkUniqueID AUDIOLANDMARKS = 3587124628U;
+                static const AkUniqueID BACKWARDS = 3876169137U;
+                static const AkUniqueID COLLISIONS = 824618274U;
+                static const AkUniqueID CONTCOLLISIONS = 1010793822U;
+                static const AkUniqueID ECHOCLICK = 3357006482U;
+                static const AkUniqueID ENDTUTORIAL = 2907714354U;
+                static const AkUniqueID ENTERINGCATEGORY = 2202139043U;
+                static const AkUniqueID FORWARDMOVEMENT = 21835643U;
+                static const AkUniqueID HEARINGTHEOBS = 2315122162U;
+                static const AkUniqueID MAINCHAMBERSPAWN = 3754331821U;
+                static const AkUniqueID MVMTPRACTICE = 3345188236U;
+                static const AkUniqueID NAVIGATINGSUBMENUS = 809063863U;
+                static const AkUniqueID OBSCUSTOM = 2131676804U;
+                static const AkUniqueID OBSNAVIGATION = 4042290811U;
+                static const AkUniqueID OBSPRESETDESC = 3519150243U;
+                static const AkUniqueID OBSPRESETS = 2312210939U;
+                static const AkUniqueID OBSREVIEW = 2115823773U;
+                static const AkUniqueID REVIEW = 30880413U;
+                static const AkUniqueID ROTATIONANDMVMTPRACTICE = 2110140363U;
+                static const AkUniqueID ROTATIONINTRO = 1521432287U;
+                static const AkUniqueID SIDEWAYS = 2818475040U;
+                static const AkUniqueID SONICCOMPASS = 3211240269U;
+                static const AkUniqueID SPAWNANDMOVE = 4054219830U;
+                static const AkUniqueID SPAWNSTART = 359097558U;
+                static const AkUniqueID STARTTUTORIAL = 4186867365U;
+                static const AkUniqueID TUTORIALITEMS = 966916481U;
+                static const AkUniqueID UIINTRO = 3193406139U;
+                static const AkUniqueID UIMENUNAV = 3765040673U;
+                static const AkUniqueID UIREVIEW = 1527458955U;
+                static const AkUniqueID WELCOME = 2421980327U;
+            } // namespace SWITCH
+        } // namespace SG_UI_TUTORIALITEMS
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID CLICK_AMPLITUDE = 250675185U;
+        static const AkUniqueID CLICK_PITCH = 4151021112U;
+        static const AkUniqueID FOOTSTEPS_AMPLITUDE = 732487008U;
+        static const AkUniqueID FOOTSTEPS_PITCH = 2809259425U;
+        static const AkUniqueID LANDMARKS_CLOCK_AMPLITUDE = 548657501U;
+        static const AkUniqueID LANDMARKS_HVAC_AMPLITUDE = 3172790959U;
+        static const AkUniqueID PLAYER_YAW = 1741542810U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+        static const AkUniqueID NVDA_TUTORIAL = 3676215661U;
+        static const AkUniqueID UI_SOUNDBANK = 2454045173U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBISONIC_MAIN_MIX = 846920411U;
+        static const AkUniqueID AMBISONIC_REVERBS = 3587466022U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID NEWTUTORIAL = 2583787875U;
+        static const AkUniqueID TUTORIAL_CONTENTS_BUS = 796032403U;
+        static const AkUniqueID UI_BUS = 3247222208U;
+        static const AkUniqueID UI_SELECTION_BUS = 1350803667U;
+        static const AkUniqueID UI_WINDOW_BUS = 1991884269U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID HALLWAY01_AMBI_VERB = 3311067494U;
+        static const AkUniqueID HALLWAY02_AMBI_VERB = 867873073U;
+        static const AkUniqueID SIMPLEREFLECT = 903073294U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
