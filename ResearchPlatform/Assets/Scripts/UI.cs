eae38305c0c2c3f59e5d1014e76a642bb0c37daf
Tanish Raorane <tanishraorane4@gmail.com>
2025-12-04 18:25:36 +0000

    UI

--- ResearchPlatform/Assets/Scripts/UI.cs
+++ ResearchPlatform/Assets/Scripts/UI.cs
@@ -5,15 +5,31 @@ public class UI : MonoBehaviour
 {
 
     public GameObject[] pages;
-    public GameObject[] UIParents;
+    //public GameObject[] UIParents;
     public Button[] levelOneButtons;
-    public Button[] levelTwoButtons;
+    //public Button[] levelTwoButtons;
    
 
-    public GameObject[] sliders;
+    //public GameObject[] sliders;
+    private bool isPage1Open = false;
+    private bool isPage2Open = false;
+
+    private int page1OptionIndex;
+    private int prevPage1OptionIndex;
+    private int page2OptionIndex;
+    private int prevPage2OptionIndex;
+
+   
+    // 0 -> unSelected color, 1 -> selected color
+    [SerializeField]
+    private Color[] color = new Color[2];
+
+    [SerializeField]
+    private GameObject player, mainMaze;
 
     void Start()
     {
+        isPage1Open = true;
         //Disable main maze and player at start
     }
 
@@ -24,6 +40,41 @@ public class UI : MonoBehaviour
         levelOneButtons[2].onClick.AddListener(OnSaveSettingsButtonPress);
     }
 
+    private void OnEnable()
+    {
+        player.GetComponent<Navigation>().enabled = false;  
+        player.GetComponent<CompassController>().enabled = false;  
+        player.GetComponent<EcholocationClick>().enabled = false;  
+        mainMaze.SetActive(false);
+        page1OptionIndex = prevPage1OptionIndex;
+    }
+
+    private void OnDisable()
+    {
+        player.GetComponent<Navigation>().enabled = true;
+        player.GetComponent<CompassController>().enabled = true;
+        player.GetComponent<EcholocationClick>().enabled = true;
+        mainMaze.SetActive(true);
+    }
+
+    private void OptionSelector(int optionIndex, ref int prevOptionIndex, Button[] levelButtons)
+    {
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            if (optionIndex == i)
+            {
+                levelButtons[i].image.color = color[1];
+                prevOptionIndex = i;
+            }
+
+            else
+            {
+                levelButtons[i].image.color = color[0];
+            }
+        }
+
+
+    }
 
   // Method to enable/disable pages
     private void Pagecontroller(int i, int j)
@@ -54,6 +105,35 @@ public class UI : MonoBehaviour
 
     void Update()
     {
-        
+        if(isPage1Open)
+        {
+           
+
+           if(Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                if(page1OptionIndex < 2)
+                {
+                    page1OptionIndex++;     //2A847F 42 132 126
+
+                }
+
+                OptionSelector(page1OptionIndex, ref prevPage1OptionIndex, levelOneButtons);
+
+            }
+
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                if (page1OptionIndex > 0)
+                {
+                    page1OptionIndex--;     
+
+                }
+
+                OptionSelector(page1OptionIndex, ref prevPage1OptionIndex, levelOneButtons);
+
+            }
+
+        }
     }
 }
